3989f1f6e7dabbab046cb715ddd8e0700097f5b5
Hi-Im-BB <pedrosmbb@gmail.com>
2020-12-07 18:55:57 -0300

    Finished issue

--- Questao6.sh
+++ Questao6.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
-INPUT = "Output da outra aplicacao"
-STATUS=$(echo "$INPUT" | grep "status:" | cut -d "'" -f2)
-
-if [ $STATUS != "OK" ]
+OUTPUT="$(cat /usr/lib/zabbix/externalscripts/output.yaml)"
+STATUS=$(echo "$OUTPUT" | grep "status:" | cut -d "'" -f2)
+if [ $STATUS == "OK" ]
 then
-    echo "1"
+        echo "1"
 else
-    echo "2"
+        echo "2"
+fi
